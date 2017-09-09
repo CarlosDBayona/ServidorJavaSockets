0e61fadfcf4a045cfc9b1c06720714c4a849d0f6
Carlos <Carlos@DESKTOP-QKM8J15>
2017-09-09 18:50:29 -0500

    Server Java

--- src/javaapplication3/HandlePos.java
+++ src/javaapplication3/HandlePos.java
@@ -124,7 +124,7 @@ public class HandlePos implements Runnable{
         Class.forName("com.mysql.jdbc.Driver").newInstance();
       String myUrl = "jdbc:mysql://localhost:3306/Edison";
       java.sql.Connection conn = DriverManager.getConnection(myUrl, "root", "root");
-      String query = "SELECT absdeg,hora FROM DatosEdison order by id desc limit 1";
+      String query = "SELECT estado FROM led order by id desc limit 1";
       Statement st = conn.createStatement();
       ResultSet rs = st.executeQuery(query);
       while (rs.next())
